f51336a4e40843c2c7bf9abc9b3c13381ce5c928
Shuliang Deng <shuliangdeng@ADAEA78E.its.caregroup.org>
2020-12-18 16:11:25 -0500

    modify dependencies

--- R/transfrom_dist.R
+++ R/transfrom_dist.R
@@ -1,8 +1,3 @@
-library(DT)
-library(sparkline)
-library(tidyverse)
-# library()
-
 transform_dist = function(df,nbreaks = 20){
   df %>% 
     pivot_longer(-1) %>% 
@@ -10,15 +5,15 @@ transform_dist = function(df,nbreaks = 20){
     group_by_at(1) %>% 
     summarise(
       ID = unique(name),
-      `raw value`         = spk_chr(hist(value                                           ,plot=FALSE, breaks = nbreaks)$counts, type="bar"),
-      `log(value)`        = spk_chr(hist(log(value - min(value,na.rm = TRUE) + 1)        ,plot=FALSE, breaks = nbreaks)$counts, type="bar"),
-      `scale(value)`      = spk_chr(hist(scale(value)                                    ,plot=FALSE, breaks = nbreaks)$counts, type="bar"),
-      `scale(log(value))` = spk_chr(hist(scale(log(value - min(value,na.rm = TRUE) + 1)) ,plot=FALSE, breaks = nbreaks)$counts, type="bar"), 
+      `raw value`         = sparkline::spk_chr(hist(value                                           ,plot=FALSE, breaks = nbreaks)$counts, type="bar"),
+      `log(value)`        = sparkline::spk_chr(hist(log(value - min(value,na.rm = TRUE) + 1)        ,plot=FALSE, breaks = nbreaks)$counts, type="bar"),
+      `scale(value)`      = sparkline::spk_chr(hist(scale(value)                                    ,plot=FALSE, breaks = nbreaks)$counts, type="bar"),
+      `scale(log(value))` = sparkline::spk_chr(hist(scale(log(value - min(value,na.rm = TRUE) + 1)) ,plot=FALSE, breaks = nbreaks)$counts, type="bar"), 
       .groups = 'drop'
     ) %>% 
     select(-name) %>% 
     ungroup() %>% 
-    datatable(
+    DT::datatable(
       escape = FALSE,
       options = list(fnDrawCallback = htmlwidgets::JS('function(){HTMLWidgets.staticRender();}'))) %>%
     spk_add_deps() 
