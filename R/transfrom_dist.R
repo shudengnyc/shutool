cc985f6324b4d18efbb54328e0ffe02f338c0a3c
Shuliang Deng <shuliangdeng@ADAEA78E.its.caregroup.org>
2020-12-18 15:37:47 -0500

    add traforamtion fucntion

---
+++ R/transfrom_dist.R
@@ -0,0 +1,25 @@
+library(DT)
+library(sparkline)
+library(tidyverse)
+# library()
+
+transform_dist = function(df,nbreaks = 20){
+  df %>% 
+    pivot_longer(-1) %>% 
+    select(-1) %>% 
+    group_by_at(1) %>% 
+    summarise(
+      ID = unique(name),
+      `raw value`         = spk_chr(hist(value                                           ,plot=FALSE, breaks = nbreaks)$counts, type="bar"),
+      `log(value)`        = spk_chr(hist(log(value - min(value,na.rm = TRUE) + 1)        ,plot=FALSE, breaks = nbreaks)$counts, type="bar"),
+      `scale(value)`      = spk_chr(hist(scale(value)                                    ,plot=FALSE, breaks = nbreaks)$counts, type="bar"),
+      `scale(log(value))` = spk_chr(hist(scale(log(value - min(value,na.rm = TRUE) + 1)) ,plot=FALSE, breaks = nbreaks)$counts, type="bar"), 
+      .groups = 'drop'
+    ) %>% 
+    select(-name) %>% 
+    ungroup() %>% 
+    datatable(
+      escape = FALSE,
+      options = list(fnDrawCallback = htmlwidgets::JS('function(){HTMLWidgets.staticRender();}'))) %>%
+    spk_add_deps() 
+}
