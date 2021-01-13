bea79e9f88d09ec2ce91b92d4edbb337d7b9e3e2
Shuliang Deng <shuliangdeng@ADAEA78E.its.caregroup.org>
2021-01-13 12:50:36 -0500

    improve peek

--- R/peek.R
+++ R/peek.R
@@ -1,1 +1,7 @@
-peek = function(df,row=5,col=10){df[1:row,1:col]}
+peek = function(df, row = 5, col = 10) {
+  column_range = ifelse(ncol(df) > col, col, ncol(df))
+  df[c(1:row, (nrow(df) - row):nrow(df)), 1:column_range]
+}
+
+
+
