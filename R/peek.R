d08302f52f00d7a0f8566c1a74fb5f2df577b2b9
Shuliang Deng <shuliangdeng@ADAEA78E.its.caregroup.org>
2021-01-13 14:51:30 -0500

    - add package desc

--- R/peek.R
+++ R/peek.R
@@ -1,7 +1,8 @@
-peek = function(df, row = 5, col = 10) {
+peek = function(df, row = 4, col = 10) {
   column_range = ifelse(ncol(df) > col, col, ncol(df))
-  df[c(1:row, (nrow(df) - row):nrow(df)), 1:column_range]
+  df[c(1:row, (nrow(df) - row + 1):nrow(df)), 1:column_range]
 }
+
 
 
 
