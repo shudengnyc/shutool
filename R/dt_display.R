bddab1f3d50c024ab3aae6ace3c030e31ca50e53
shudengnyc <42967629+shudengnyc@users.noreply.github.com>
2022-12-23 09:18:37 -0500

    restart improving

--- R/dt_display.R
+++ R/dt_display.R
@@ -1,6 +1,7 @@
 dt_display = 
   function(df, file_name = "Downloaded Report",...){
     df %>% 
+      dplyr::mutate_if(is.numeric, signif, 3) %>%
       DT::datatable(
         rownames = FALSE,
         ...,
@@ -8,14 +9,8 @@ dt_display =
         options = list(
           paging = TRUE,searching = TRUE,fixedColumns = TRUE,autoWidth = TRUE,ordering = TRUE,
           dom = 'ftBip', #Bfrtip
-          buttons = list(
-            'copy',
-            list(
-              extend = 'collection',
-              buttons = list(extend = 'csv', filename = paste(file_name,Sys.Date())),
-              text = 'Download'
-            )
-          )
+          buttons = list(c('csv', 'excel'))
+          # buttons = list('copy',list(extend = 'collection',buttons = list(extend = 'csv', filename = paste(file_name, Sys.Date()))))
         )
       )
   }
